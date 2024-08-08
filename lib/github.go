51ba5e4cf4dbf036f22dbf267e52f8efaddedabc
JackBekket <jackatwork@yandex.ru>
2024-08-08 21:48:51 +0400

    github

---
+++ lib/github.go
@@ -0,0 +1,34 @@
+package github
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	. "github.com/go-git/go-git/v5/_examples"
+)
+
+// Basic example of how to clone a repository using clone options.
+func Clone(url_origin string, dir string) {
+	CheckArgs(url_origin, dir)
+	url := url_origin
+	directory := dir
+
+	// Clone the given repository to the given directory
+	Info("git clone %s %s --recursive", url, directory)
+
+	r, err := git.PlainClone(directory, false, &git.CloneOptions{
+		URL:               url,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	})
+
+	CheckIfError(err)
+
+	// ... retrieving the branch being pointed by HEAD
+	ref, err := r.Head()
+	CheckIfError(err)
+	// ... retrieving the commit object
+	commit, err := r.CommitObject(ref.Hash())
+	CheckIfError(err)
+
+	fmt.Println(commit)
+}
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	gh "github.com/JackBekket/Reflexia/github"
 	"github.com/joho/godotenv"
 )
 
@@ -29,6 +30,7 @@ func main() {
     }
 
     os.Mkdir("temp", os.ModePerm)
+	gh.Clone(repoLink,"/temp/")
     cmd := exec.Command("git", "clone", repoLink, "temp")
     err = cmd.Run()
     if err != nil {
